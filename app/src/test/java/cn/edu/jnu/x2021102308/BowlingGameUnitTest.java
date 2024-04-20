e3a1729cc6e38205aa687305156f4693ec422589
XiaofengQiu <18320651165@163.com>
2024-04-20 09:42:43 +0800

    test: add test case 5
    测试五，12个10分

--- app/src/test/java/cn/edu/jnu/x2021102308/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2021102308/BowlingGameUnitTest.java
@@ -51,7 +51,7 @@ public class BowlingGameUnitTest {
     }
 
     @Test
-    public void testTwoSpare() {
+    public void testTwoStrike() {
         rollOneStrike();
         rollOneStrike();
         repeatedRoll(1, 2);
@@ -59,5 +59,15 @@ public class BowlingGameUnitTest {
         assertEquals(35, game.score());
     }
 
+    @Test
+    public void testAllStrike() {
+        rollStrikes(12);
+        assertEquals(300, game.score());
+    }
 
+    private void rollStrikes(int times) {
+        for (int i = 0; i < times; i++) {
+            rollOneStrike();
+        }
+    }
 }
