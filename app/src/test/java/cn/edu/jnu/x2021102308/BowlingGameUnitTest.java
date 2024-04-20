0441bba950b0b011ffdcfe82a4336fb9134f3eea
XiaofengQiu <18320651165@163.com>
2024-04-20 08:44:00 +0800

    test: add test 1
    测试一，20个0

---
+++ app/src/test/java/cn/edu/jnu/x2021102308/BowlingGameUnitTest.java
@@ -0,0 +1,21 @@
+package cn.edu.jnu.x2021102308;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class BowlingGameUnitTest {
+    @Test
+    public void testAllZeros() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
+}
