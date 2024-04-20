e83f4d3f63f58bab2c1c53d8a2968b2726b3c59a
XiaofengQiu <18320651165@163.com>
2024-04-20 09:17:13 +0800

    fix: fix code to pass test 3
    修改代码，使其能通过 test case 3

--- app/src/main/java/cn/edu/jnu/x2021102308/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2021102308/BowlingGame.java
@@ -1,13 +1,27 @@
 package cn.edu.jnu.x2021102308;
 
+import java.util.ArrayList;
+
+
 public class BowlingGame {
-    private int score = 0;
+    private ArrayList<Integer> pins;
+
+    public BowlingGame() {
+        pins = new ArrayList<>();
+    }
 
     public void roll(int pins) {
-        score += pins;
+        this.pins.add(pins);
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < pins.size(); i++) {
+            score += pins.get(i);
+            if (i + 2 < pins.size() && pins.get(i) + pins.get(i + 1) == 10) {
+                score += pins.get(i + 2);
+            }
+        }
         return score;
     }
 }
