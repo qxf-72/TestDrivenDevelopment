b83060bfe2c1dde814dc55488f4f193eb0a9389d
XiaofengQiu <18320651165@163.com>
2024-04-20 09:45:13 +0800

    fix: fix code to pass test case 5
    修改代码，使其能通过test case 5

--- app/src/main/java/cn/edu/jnu/x2021102308/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2021102308/BowlingGame.java
@@ -1,7 +1,7 @@
 package cn.edu.jnu.x2021102308;
 
 public class BowlingGame {
-    private int rolls[] = new int[21];
+    private final int[] rolls = new int[21];
     private int currentRoll = 0;
 
 
@@ -14,13 +14,13 @@ public class BowlingGame {
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
             if (isStrike(frameIndex)) {
-                score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                score += 10 + strikeBonus(frameIndex);
                 frameIndex++;
             } else if (isSpare(frameIndex)) {
-                score += 10 + rolls[frameIndex + 2];
+                score += 10 + spareBonus(frameIndex);
                 frameIndex += 2;
             } else {
-                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                score += sumOfBallsInFrame(frameIndex);
                 frameIndex += 2;
             }
         }
@@ -33,5 +33,17 @@ public class BowlingGame {
 
     boolean isSpare(int frameIndex) {
         return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
     }
 }
