aa1ddf4d519f4a089d63ac9ca9bd4bb00d9be78a
XiaofengQiu <18320651165@163.com>
2024-04-20 09:00:08 +0800

    test: add test 2
    测试二，20个1

--- app/src/test/java/cn/edu/jnu/x2021102308/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2021102308/BowlingGameUnitTest.java
@@ -2,6 +2,7 @@ package cn.edu.jnu.x2021102308;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,12 +11,28 @@ import org.junit.Test;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class BowlingGameUnitTest {
+    private BowlingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+    }
+
     @Test
     public void testAllZeros() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        repeatedRoll(0, 20);
         assertEquals(0, game.score());
+    }
+
+    @Test
+    public void testAllOnes() {
+        repeatedRoll(1, 20);
+        assertEquals(20, game.score());
+    }
+
+    private void repeatedRoll(int pins, int times) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
     }
 }
