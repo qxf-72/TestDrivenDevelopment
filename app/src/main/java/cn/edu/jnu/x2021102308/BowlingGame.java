ac075070e693375e4f3e12adf70a8032d5913ad3
XiaofengQiu <18320651165@163.com>
2024-04-20 09:02:20 +0800

    fix: fix code to pass test 2
    更新代码，使其能通过test 2

--- app/src/main/java/cn/edu/jnu/x2021102308/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2021102308/BowlingGame.java
@@ -1,10 +1,13 @@
 package cn.edu.jnu.x2021102308;
 
 public class BowlingGame {
+    private int score = 0;
+
     public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
